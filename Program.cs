3789752ab374a35874365c600285755ecd5deb4e
AhmedMunir-1 <am01227288206@gmail.com>
2025-11-20 06:24:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//cleaning services
+Dictionary<string,double> carpetPrice = new Dictionary<string, double>() {{"small",25}, {"large",35},{"Taxes",0.06},{"estimatValid",30}};
+
+Console.WriteLine("========================");
+
+Console.Write("Please enter Num of Small Carbets : ");
+int SmallCarbetNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter Num of Large Carbets : ");
+int LargeCarpetNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Price per small carpets: $25");
+Console.WriteLine($"Price per large carpets: $35");
+
+double totalCost = (SmallCarbetNum*carpetPrice["small"])+(LargeCarpetNum*carpetPrice["large"]);
+Console.WriteLine($"Cost : {totalCost:F2}");
+
+double totalTaxes = totalCost*carpetPrice["Taxes"];
+Console.WriteLine($"Tax : {totalTaxes:F2}");
+
+Console.WriteLine("========================");
+
+Console.WriteLine($"Total estimate: {totalCost+totalTaxes:F2}");
+Console.WriteLine($"This estimate is valid for {carpetPrice["estimatValid"]}");
